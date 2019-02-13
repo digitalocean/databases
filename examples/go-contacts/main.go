30518d79b9bd1d704ddc6e55482289ba9af2f6ee
Kamal Nasser <hello@kamal.io>
2019-02-13 21:46:48 +0200

    minor adjustments to go-contacts (#6)

--- examples/go-contacts/main.go
+++ examples/go-contacts/main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"encoding/json"
 	"flag"
-	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strings"
 
 	_ "github.com/jackc/pgx/stdlib"
@@ -32,6 +33,13 @@ type Contact struct {
 	UpdatedAt string `db:"updated_at"`
 }
 
+var (
+	connectionString = flag.String("conn", getenvWithDefault("DATABASE_URL", ""), "PostgreSQL connection string")
+	listenAddr       = flag.String("addr", getenvWithDefault("LISTENADDR", ":8080"), "HTTP address to listen on")
+	db               *sqlx.DB
+	tmpl             = template.New("")
+)
+
 func getenvWithDefault(name, defaultValue string) string {
 	val := os.Getenv(name)
 	if val == "" {
@@ -41,13 +49,6 @@ func getenvWithDefault(name, defaultValue string) string {
 	return val
 }
 
-var (
-	connectionString = flag.String("conn", getenvWithDefault("DATABASE_URL", ""), "PostgreSQL connection string")
-	listenAddr       = flag.String("addr", getenvWithDefault("LISTENADDR", ":8080"), "HTTP address to listen on")
-	db               *sqlx.DB
-	tmpl             = template.New("")
-)
-
 func main() {
 	flag.Parse()
 	var err error
@@ -55,30 +56,27 @@ func main() {
 	// templating
 
 	tmpl.Funcs(template.FuncMap{"StringsJoin": strings.Join})
-	_, err = tmpl.ParseGlob("./templates/*.html")
+	_, err = tmpl.ParseGlob(filepath.Join(".", "templates", "*.html"))
 	if err != nil {
-		fmt.Printf("Unable to parse templates: %v\n", err)
-		return
+		log.Fatalf("Unable to parse templates: %v\n", err)
 	}
 
 	// postgres connection
 
 	if *connectionString == "" {
-		fmt.Println("Please pass the connection string using the -conn option")
-		return
+		log.Fatalln("Please pass the connection string using the -conn option")
 	}
 
 	db, err = sqlx.Connect("pgx", *connectionString)
 	if err != nil {
-		fmt.Printf("Unable to establish connection: %v\n", err)
-		return
+		log.Fatalf("Unable to establish connection: %v\n", err)
 	}
 
 	// http server
 
 	http.HandleFunc("/", handler)
 
-	fmt.Printf("listening on %s\n", *listenAddr)
+	log.Printf("listening on %s\n", *listenAddr)
 	http.ListenAndServe(*listenAddr, nil)
 }
 
