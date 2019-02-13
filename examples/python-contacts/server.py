b47592b6acafd4cc17e43f23b0e6d1bc03e45bcb
Kenneth Reitz <me@kennethreitz.org>
2019-02-13 10:11:28 -0500

    working!

--- examples/python-contacts/server.py
+++ examples/python-contacts/server.py
@@ -1,7 +1,24 @@
 import os
 
+import responder
 from records import Database
 
 DATABASE_URL = os.environ["DATABASE_URL"]
 
 db = Database()
+api = responder.API()
+
+
+def migrate(db):
+    db.query_file("../contacts.sql")
+
+
+@api.route("/")
+async def greet_world(req, resp):
+    contacts = db.query("SELECT * FROM contacts")
+    resp.text = api.template("index.html", contacts=contacts)
+
+
+if __name__ == "__main__":
+    migrate(db=db)
+    api.run()
